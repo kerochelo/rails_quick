646c9d98b560965bb6df90d2509b9c4434161861
Takahiro Kanno <kerochelo@gmail.com>
2020-07-25 18:54:01 +0900

    [add]taskのcrud追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,28 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "create 「#{task.name}」"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
